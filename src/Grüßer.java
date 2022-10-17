4ba46ca865f33577e94ed8e0083eee860249fb9a
Josef Schaitl <josef.schaitl@gmail.com>
2022-10-17 19:38:50 +0200

    First draft

---
+++ src/Grüßer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+class Grüßer {
+    static String[] Grüße = {"Hallo", "Griasdi", "Servas", "Guten Morgen", "Hi", "Herzlich Willkommen"};
+
+    public static void main(String[] args) throws IOException {
+        Path Datei = Path.of(args[0]);
+        List<String> Zeilen = Files.readAllLines(Datei);
+        List<String> Vornamensliste = new ArrayList<>();
+        for (String Zeile : Zeilen) {
+            String[] Teile = Zeile.split(" ");
+            if (Teile.length < 1) {
+                continue;
+            }
+            Vornamensliste.add(Teile[0]);
+        }
+        for (String Vorname : Vornamensliste) {
+            final int WelcherGruß = ThreadLocalRandom.current().nextInt(Grüße.length);
+            final String Gruß = Grüße[WelcherGruß] + " ";
+            System.out.println(Gruß + Vorname + "!");
+        }
+    }
+}
